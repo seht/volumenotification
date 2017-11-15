c90e2f88426d0b33d85299cf613bfcd6ed3335be
seht <seht@users.noreply.github.com>
2017-11-15 00:05:00 +0000

    updated copyright, removed from resource files

--- app/src/main/java/net/hyx/app/volumenotification/activity/SettingsActivity.java
+++ app/src/main/java/net/hyx/app/volumenotification/activity/SettingsActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Seth Montenegro
+ * Copyright 2017 https://github.com/seht
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
