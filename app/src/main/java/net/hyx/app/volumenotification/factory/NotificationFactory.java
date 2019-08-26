02d10d88375fef9c03fb92b99731c6da4176d946
seht <seht@hyx.net>
2019-08-26 09:14:07 +0200

    notification to flag_update_current

--- app/src/main/java/net/hyx/app/volumenotification/factory/NotificationFactory.java
+++ app/src/main/java/net/hyx/app/volumenotification/factory/NotificationFactory.java
@@ -148,7 +148,7 @@ public class NotificationFactory {
             RemoteViews btn = new RemoteViews(packageName, R.layout.view_widget_volume_control);
             PendingIntent event = PendingIntent.getBroadcast(context, item.type,
                     new Intent(context, AdjustVolumeReceiver.class).putExtra(VolumeControlModel.STREAM_TYPE_FIELD, item.type),
-                    PendingIntent.FLAG_CANCEL_CURRENT);
+                    PendingIntent.FLAG_UPDATE_CURRENT);
 
             btn.setOnClickPendingIntent(R.id.btn_volume_control, event);
             btn.setInt(R.id.btn_volume_control, "setImageResource", volumeControlModel.getIconId(item.icon));
