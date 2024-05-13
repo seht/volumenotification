bcf43b301a6f2200f2bd5d71919e05149dbe5545
seht <seht@hyx.net>
2024-05-13 15:42:34 +0000

    merge tiles

--- app/src/main/java/net/hyx/app/volumenotification/controller/TileServiceController.java
+++ app/src/main/java/net/hyx/app/volumenotification/controller/TileServiceController.java
@@ -35,6 +35,7 @@ import net.hyx.app.volumenotification.service.TileServiceNotificationVolume;
 import net.hyx.app.volumenotification.service.TileServiceRingVolume;
 import net.hyx.app.volumenotification.service.TileServiceSystemVolume;
 
+@TargetApi(Build.VERSION_CODES.N)
 public class TileServiceController {
 
     private final Context context;
@@ -50,12 +51,10 @@ public class TileServiceController {
     }
 
     public void requestListening() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            requestListeningTiles();
-        }
+        requestListeningTiles();
     }
 
-    @TargetApi(Build.VERSION_CODES.N)
+
     private void requestListeningTiles() {
         String[] tileServices = {
                 TileServiceMediaVolume.class.getName(),
@@ -72,7 +71,6 @@ public class TileServiceController {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.N)
     public void updateTile(Tile tile, int streamType) {
         VolumeControl item = volumeControlModel.getItemByType(streamType);
         if (item == null) {
@@ -80,7 +78,11 @@ public class TileServiceController {
         }
         tile.setIcon(Icon.createWithResource(context, volumeControlModel.getIconId(item.icon)));
         tile.setLabel(item.label);
-        tile.setState(Tile.STATE_ACTIVE);
+        if (item.status == 1) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
         tile.updateTile();
     }
 
